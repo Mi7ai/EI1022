1d4b4018c24100abe769223ca4a35d97424fa55b
mi7ai <hottmayer@gmail.com>
2018-10-03 14:06:31 +0200

    Path between 2 cells

--- L2/L2Ex14.py
+++ L2/L2Ex14.py
@@ -1,4 +1,5 @@
-from L2.L2Ex11 import first,take_while
+from L2.L2Ex11 import first
+from L2.L2Ex13 import take_while
 def squares():
     n=1
     while True:
@@ -11,7 +12,7 @@ def escapicua(n):
     return a==b
 
 a = first(100,squares())
-b = take_while(lambda n: ,squares())
+b = take_while(lambda n: n<10 ,squares())
 c = first(10, filter(escapicua,squares()))
 
 print(list(c))
