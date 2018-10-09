90c2b59556127950b0fa0bebb47262d10240f2fd
mi7ai <hottmayer@gmail.com>
2018-10-09 15:31:24 +0200

    Entregable 1

--- E1/entregable1.py
+++ E1/entregable1.py
@@ -1,8 +1,10 @@
 import sys
 from algoritmia.datastructures.digraphs import UndirectedGraph
+from labyrinthviewer import LabyrinthViewer
 
 
-print("El nombre del programa:", sys.argv[0])
+
+
 def load_labyrinth(nombre_fichero):
     vertices = set()
     f = 0  # f = fila
@@ -12,13 +14,14 @@ def load_labyrinth(nombre_fichero):
             muro = celdas[c]  # contiene info de donde estan los muros,wsn,se,etc...
 
             if 's' not in muro:
-                vertices.append(((f + 1), (c)))
+                vertices.add(((f + 1), (c)))
             if 'n' not in muro:
-                vertices.append(((f - 1), (c)))
+                vertices.add(((f - 1), (c)))
             if 'e' not in muro:
-                vertices.append(( f, (c + 1)))
+                vertices.add(( f, (c + 1)))
             if 'w' not in muro:
-                vertices.append((f, (c - 1)))
+                vertices.add((f, (c - 1)))
+
 
     f += 1
 
@@ -26,11 +29,10 @@ def load_labyrinth(nombre_fichero):
 
 
 
+if __name__ == '__main__':
 
-    num_rows = 8
-    num_cols = 8
 
     lab = load_labyrinth(sys.argv[1])
     print(lab)
-    viewer = labyrinthviewer(lab, canvas_width=800, canvas_height=480, margin=10)
+    viewer = Labyrinthviewer(lab, canvas_width=800, canvas_height=480, margin=10)
     viewer.run()
