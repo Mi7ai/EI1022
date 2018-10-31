28ccb0ae3726a912ab52d55a7e7ae24732a73c1c
mi7ai <hottmayer@gmail.com>
2018-10-31 14:58:23 +0100

    sudoku acabado

--- Utils/sudoku.py
+++ Utils/sudoku.py
@@ -1,5 +1,6 @@
 from Utils.bt_scheme import PartialSolution, BacktrackingSolver
 from typing import *
+from copy import deepcopy
 
 Position = Tuple[int, int]
 Sudoku = List[List[int]]
@@ -11,6 +12,9 @@ def primera_vacia(s: Sudoku) -> Optional[Position]:
             if s[fila][col] == 0:
                 return fila, col
     return None  # si el Sudoku ya está completo
+
+def vacias(s: Sudoku)-> List[Position]:
+    return [(fila, col) for fila in range(9) for col in range(9)if s[fila][col] == 0] #lo mismo que arriba mas o menos solo que devuelve la lista vacia si cumple el if
 
 
 def posibles_en(s: Sudoku, fila: int, col: int) -> Set[int]:
@@ -33,21 +37,46 @@ def pretty_print(s: Sudoku):
 
 
 class SudokuPS(PartialSolution):
-    def __init__(self, sudoku: Sudoku):
+    def __init__(self, sudoku: Sudoku, vacias: List[Position]):
+        self.vacias = vacias
         self.s = sudoku
+        sn = deepcopy(self.s)
 
     # Indica si la sol. parcial es ya una solución factible (completa)
     def is_solution(self) -> bool:
-        pass  # IMPLEMENTAR
+        # return primera_vacia(self.s) is None
+        return len(vacias(self.s)) == 0
 
     # Si es sol. factible, la devuelve. Si no lanza excepción
     def get_solution(self) -> Sudoku:
-        pass  # IMPLEMENTAR
+        # if not self.is_solution(): raise RuntimeError("No hay solucion!")
+        if self.is_solution() == False:
+            raise RuntimeError("No hay solucion!")
+        return self.s
 
     # Devuelve la lista de sus sol. parciales sucesoras
     def successors(self) -> Iterable["SudokuPS"]:
-        pass  # IMPLEMENTAR
-
+        # pos = primera_vacia(self.s)
+        # if pos is None: return
+        # f, c = pos
+        #
+        # for num in posibles_en(self.s, f, c):
+        #     sudoku_nuevo = deepcopy(self.s)
+        #     sudoku_nuevo[f][c] = num
+        #     yield SudokuPS(sudoku_nuevo)
+        # -------------------------
+         mejor_pos = argmin(self.vacias, lambda pos: len(posibles_en(self.s, pos[0], pos[1])))
+        # esto se cambbia por pos de arriba y se añaden las 2 lineas de vacias_nueva
+        # -------------------------
+        if mejor_pos(self.s) is None: return
+        f, c = mejor_pos
+        vacias_nueva = deepcopy(self.vacias)
+        vacias_nueva.remove(mejor_pos)
+        for num in posibles_en(self.s, f, c):
+                sudoku_nuevo[f][c] = num
+                yield SudokuPS(sudoku_nuevo,vacias)
+        sudoku_nuevo[f][c] = 0
+        self.vacias.append(mejor_pos)
 
 # PROGRAMA PRINCIPAL -------------------------------------------------------
 if __name__ == "__main__":
@@ -65,6 +94,8 @@ if __name__ == "__main__":
     print("\nSoluciones:")
     # Mostrar todas las soluciones
     # IMPLEMENTAR utilizando SudokuPS y BacktrackingSolver
-    # for solution in ...:
-    #     pretty_print(solution)
+
+    initial_ps = SudokuPS(m_sudoku) # objeto que solo usa los 3 metodos de arriba
+    for solution in BacktrackingSolver.solve(initial_ps):
+        pretty_print(solution)
     print("<TERMINDADO>")  # Espera a ver este mensaje para saber que el programa ha terminado
